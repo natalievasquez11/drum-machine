fe08ff4aad28b046a49bf0a5956ce0c4cf8a7590
Natalie Vasquez <natalievasquez11@gmail.com>
2021-10-13 15:48:28 -0500

    added audio functionality

--- src/components/drum-pad/DrumPad.js
+++ src/components/drum-pad/DrumPad.js
@@ -1,13 +1,27 @@
 import './drumPad.css';
+import clap1 from '../../audio/Clap-Neat.wav';
+import { Howl, Howler } from 'howler';
+
+
+const audioClip = {
+  sound: clap1,
+  label: "B"
+}
 
 function DrumPad() {
-  let handleDrumSound = () => {
-    console.log('click works');
+
+  let soundPlay = (src) => {
+    const sound = new Howl({
+      src
+    });
+    sound.play();
   }
 
+  Howler.volume(1.0);
+
   return(
-    <div className="drum-pad" onClick={handleDrumSound}>
-        <p>A</p>
+    <div className="drum-pad" onClick={() => soundPlay(audioClip.sound)}>
+      <p>{audioClip.label}</p>
     </div>
   )
 }
