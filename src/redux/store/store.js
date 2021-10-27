dab888728af2169ebd1ea33f83755042f6665e6a
Natalie Vasquez <natalievasquez11@gmail.com>
2021-10-27 17:51:38 -0500

    redux updates

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-import rootReducer from './reducers/audioClipReducer';
+import rootReducer from '../reducers/audioClipReducer';
 
 const store = createStore(rootReducer);
 
