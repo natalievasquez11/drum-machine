a8e0a510f97fb434d52162cead667d15ad4c9cb9
Natalie Vasquez <natalievasquez11@gmail.com>
2021-10-27 13:57:51 -0500

    update reducer

--- src/store/reducers/audioClipReducer.js
+++ src/store/reducers/audioClipReducer.js
@@ -1,9 +1,17 @@
+import { UPDATE_AUDIO } from '../constants/action-types';
+
 const initialState = {
   currentAudioClip: {}
 };
 
 function rootReducer(state = initialState, action) {
-  return state;
+  switch(action.type) {
+    case UPDATE_AUDIO:
+      return {...state, currentAudioClip: action.payload}
+    default:
+      return state;
+  }
+
 }
 
 export default rootReducer;
